e38ddfb17029f55377eb37522189b92622d531bd
Sumit Pandey <68957789+sumi1pandey@users.noreply.github.com>
2024-01-31 23:20:37 +0530

    Update Rock, Paper, Scissor Game.py

--- Rock, Paper, Scissor Game.py
+++ Rock, Paper, Scissor Game.py
@@ -16,5 +16,5 @@ def play():
 def is_win(player, opponent):
     if ( player == 'r' and opponent == 's') or ( player == 's' and opponent == 'p') or ( player == 'p' and opponent == 'r') :
         return True
-
+        
 play()
